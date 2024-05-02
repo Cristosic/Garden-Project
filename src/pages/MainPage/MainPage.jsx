5e1917a8e31499f63c5b7337dee299770b419405
Wadim <ryl.wadim@gmail.com>
2024-05-02 14:36:22 +0200

    add HeaderBanner

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import styles from "./MainPage.module.css";
+import HeaderBanner from '../../components/HeaderBanner/Banner';
 import SectionSale from '../../components/SectionSale/SectionSale';
+
 
 export default function MainPage() {
   return (
     <div className={styles}>
+      <HeaderBanner/>
       <SectionSale/>
+      
     </div>
   )
 }
