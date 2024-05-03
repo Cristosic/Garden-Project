fcf7726d74b7517b537741554f5429fb767436ff
Cristina <acristina4040@gmail.com>
2024-05-03 17:23:46 +0200

    add navBar with Links to MainPage/CategoriesPage/AllSalePage/AllProduct and styles

--- src/components/SectionSale/SaleProductsCard/SaleProductsCard.jsx
+++ src/components/SectionSale/SaleProductsCard/SaleProductsCard.jsx
@@ -5,25 +5,23 @@ import bag from "../../../media/icons/bag.svg";
 
 function SaleProductsCard({ id, title, image, price, discont_price }) {
   return (
-    
-      <div className={styles.cardContent}>
-        <div className={styles.cardImg}>
-          <img src={image} alt={title} />
-          <div className={styles.discountLabel}>
-            -{Math.round(100 - (discont_price / price) * 100)}%
-          </div>
-          <div className={styles.cardIcons}>
-            <img src={heart} alt="" className={styles.heart} />
-            <img src={bag} alt="" className={styles.shoppingBag} />
-          </div>
+    <div className={styles.cardContent}>
+      <div className={styles.cardImg}>
+        <img src={image} alt={title} />
+        <div className={styles.discountLabel}>
+          -{Math.round(100 - (discont_price / price) * 100)}%
         </div>
-        <h4>{title}</h4>
-        <div className={styles.priceContainer}>
-          <p>${Math.round(price)}</p>
-          <p>${Math.round(discont_price)}</p>
+        <div className={styles.cardIcons}>
+          <img src={heart} alt="" className={styles.heart} />
+          <img src={bag} alt="" className={styles.shoppingBag} />
         </div>
       </div>
-    
+      <h4>{title}</h4>
+      <div className={styles.priceContainer}>
+        <p>${Math.round(price)}</p>
+        <p>${Math.round(discont_price)}</p>
+      </div>
+    </div>
   );
 }
 
