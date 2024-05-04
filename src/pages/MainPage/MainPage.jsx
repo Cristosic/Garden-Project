6ea681ba67aae52a385ddea78ec391e383a6cc2f
Cristina <acristina4040@gmail.com>
2024-05-04 22:58:23 +0200

    resolve problems with branches and conflicts

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import styles from "./MainPage.module.css";
+import SectionSale from '../../components/SectionSale/SectionSale';
+import CategoriesContainer from '../../components/CategoriesSection/CategoriesContainer/CategoriesContainer';
 
 export default function MainPage() {
   return (
-    <div className={styles}>MainPage</div>
+    <div className={styles}>
+      <CategoriesContainer />
+      <SectionSale/>
+    </div>
   )
 }
