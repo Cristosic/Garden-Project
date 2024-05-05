dcae030d98af848522e0466c1c70dab35c45596f
Gismo <ryl.wadim@gmail.com>
2024-05-05 20:38:09 +0200

    add Main Page Banner

---
+++ src/components/HeaderBanner/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import banner from '../../media/images/banner.svg';
+import styles from '../HeaderBanner/Banner.module.css';
+
+const Banner = () => {
+  return (
+
+
+    <div className={styles.banner} >
+      <div>
+        <img className={styles.headerimg} src={banner} alt="" />
+      </div>
+      <div className=''></div>
+      <h1 className={styles.title}>Amazing Discounts <br /> on Garden Products!</h1>
+      <Link to="/sales" className={styles.button}>Check out</Link>
+    </div>
+  );
+}
+
+export default Banner;
+
+
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from "./MainPage.module.css";
+import Banner from '../../components/HeaderBanner/Banner';
 import SectionSale from '../../components/SectionSale/SectionSale';
 import CategoriesContainer from '../../components/CategoriesSection/CategoriesContainer/CategoriesContainer';
 
 export default function MainPage() {
   return (
     <div className={styles}>
+      <Banner />
       <CategoriesContainer />
-      <SectionSale/>
+      <SectionSale />
     </div>
   )
 }
