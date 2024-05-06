3809cb1965e9e89337cff6f1757502aa9df0eb44
Your Name <you@example.com>
2024-05-06 11:29:29 +0200

    delete padding from main container, add padding to categoriesContainer and add right path for categories img

--- src/components/CategoriesSection/CategoryCard/CategoryCard.jsx
+++ src/components/CategoriesSection/CategoryCard/CategoryCard.jsx
@@ -4,7 +4,7 @@ import styles from "./CategoryCard.module.css";
 export default function CategoryCard({ id, title, image }) {
   return (
     <div className={styles.cardContent}>
-      <div className={styles.cardImg}>
+      <div className={`http://localhost:3333${image}`}>
         <img className={styles.categoriesImg} src={image} alt={title} />
       </div>
       <h4 className={styles.cardTitle}>{title}</h4>
