8a2692f3e1a6bb3b51353448a1a75c85a0654831
Your Name <you@example.com>
2024-05-06 12:00:54 +0200

    add correct src/components/CategoriesSection/CategoryCard/CategoryCard.jsx

--- src/components/CategoriesSection/CategoryCard/CategoryCard.jsx
+++ src/components/CategoriesSection/CategoryCard/CategoryCard.jsx
@@ -4,7 +4,7 @@ import styles from "./CategoryCard.module.css";
 export default function CategoryCard({ id, title, image }) {
   return (
     <div className={styles.cardContent}>
-      <div className={styles.categoriesImg}>
+      <div className={styles.cardImg}>
         <img className={styles.categoriesImg} src={`http://localhost:3333${image}`} alt={title} />
       </div>
       <h4 className={styles.cardTitle}>{title}</h4>
