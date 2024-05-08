fc004929fe8c99793945ca80a1bffd3bb9c88761
Cristina <acristina4040@gmail.com>
2024-05-08 21:30:56 +0200

    add new styles and links to heart and cart icons

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 import styles from "./MainPage.module.css";
-import SectionSale from '../../components/SectionSale/SectionSale';
-import FormDiscount from '../../components/DiscountForm/FormDiscount/FormDiscount';
-import CategoriesContainer from '../../components/CategoriesSection/CategoriesContainer/CategoriesContainer';
+import SectionSale from "../../components/SectionSale/SectionSale";
+import FormDiscount from "../../components/DiscountForm/FormDiscount/FormDiscount";
+import CategoriesContainer from "../../components/CategoriesSection/CategoriesContainer/CategoriesContainer";
 
 export default function MainPage() {
-
   return (
     <div className={styles}>
-      
       <CategoriesContainer />
-<FormDiscount />
-      <SectionSale/>
+      <FormDiscount />
+      <SectionSale />
     </div>
-  )
+  );
 }
