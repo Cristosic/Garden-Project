cb84ddce6c49e3cb962d0973a555be3fb1855600
Liuba <romanliuba15@gmail.com>
2024-05-09 12:37:37 +0200

    modified styles/FormDiscount

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,11 +4,9 @@ import SectionSale from '../../components/SectionSale/SectionSale';
 import CategoriesContainer from '../../components/CategoriesSection/CategoriesContainer/CategoriesContainer';
 
 export default function MainPage() {
-
-  
-
   return (
     <div className={styles}>
+      <CategoriesContainer />
       <SectionSale/>
     </div>
   )
