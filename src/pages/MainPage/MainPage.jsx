7136dcc2372e7289e1389df5e7727b5a0a038db1
Gismo <ryl.wadim@gmail.com>
2024-05-12 13:35:47 +0200

    add Media adaptiv

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "./MainPage.module.css";
+import Header from "../../components/Header/Header";
 import SectionSale from '../../components/SectionSale/SectionSale';
 import CategoriesContainer from '../../components/CategoriesSection/CategoriesContainer/CategoriesContainer';
+
 
 export default function MainPage() {
   return (
     <div className={styles}>
-      <Banner />
+     <Header />
       <CategoriesContainer />
-      <SectionSale/>
+      <SectionSale />
     </div>
   );
 }
