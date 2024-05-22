38716245cfb6ca9eea825e607b9da55cb5b22c00
Your Name <you@example.com>
2024-05-22 12:25:09 +0200

    add SingleCategoryPage

--- src/components/AllCategories/AllCategories.jsx
+++ src/components/AllCategories/AllCategories.jsx
@@ -1,20 +1,23 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategories } from "./../../store/slices/categoriesSlice";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styles from "./AllCategories.module.css";
 import CategoryCard from "../CategoriesSection/CategoryCard/CategoryCard";
 
 export default function AllCategories() {
   const dispatch = useDispatch();
+  const {categoryId} = useParams()
 
   useEffect(() => {
     dispatch(getCategories());
-  }, []);
+  }, [dispatch]);
 
   const categoriesState = useSelector(
     (state) => state.categories.categoriesData
   );
+
+  
   return (
     <div className={styles.categories_container}>
       <div className={styles.btn_links_categoriesPage}>
@@ -32,11 +35,13 @@ export default function AllCategories() {
       </div>
       <span>Categories</span>
 
+      <Link to={`/categories/${categoryId}`}>
       <div className={styles.containerImg}>
         {categoriesState.map((el) => (
           <CategoryCard key={el.id} {...el} />
         ))}
       </div>
+      </Link>
     </div>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit"
 import allProductsReducer from "./slices/allProductsSlice"
 import categoriesReducer from './slices/categoriesSlice'
 import favoritesReducer from './slices/favoritesSlice';
+import oneCategoryReducer from "./slices/oneCategorySlice";
 
 export const store = configureStore({
     reducer: {
       allProducts: allProductsReducer,
       categories: categoriesReducer,
-      favorites: favoritesReducer
+      favorites: favoritesReducer,
+      oneCategory: oneCategoryReducer
     }
   })
