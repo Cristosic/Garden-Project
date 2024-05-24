afcfb40a2f86b38822b07732de73aace6beb6307
Viktor.Gladkich <viktorgladkich7@gmail.com>
2024-05-24 23:56:30 +0200

    add filter for favorites products

--- src/components/FilterProducts/FilterProducts.jsx
+++ src/components/FilterProducts/FilterProducts.jsx
@@ -1,17 +1,21 @@
-
 import React, { useContext } from "react";
 import styles from "./FilterProducts.module.scss";
 import arrowDown from "../../media/icons/arrowDown.svg";
 import { useDispatch } from "react-redux";
-import { filterPriceAction, filterSaleProductsAction, sortProductsAction } from "../../store/slices/allProductsSlice";
 import { Context } from "../../context";
+import {
+  filterPriceAction,
+  filterSaleProductsAction,
+  sortProductsAction,
+} from "../../store/slices/allProductsSlice";
+import {
+  filterFavoritesPriceAction,
+  sortFavoritesAction,
+} from "../../store/slices/favoritesSlice";
 
-function FilterProducts({ schowSaleFilter }) {
-
-  const { theme } = useContext(Context);
-
-
+function FilterProducts({ schowSaleFilter, favoritesPage }) {
   const dispatch = useDispatch();
+  const { theme } = useContext(Context);
 
   const filter = (e) => {
     e.preventDefault();
@@ -20,10 +24,13 @@ function FilterProducts({ schowSaleFilter }) {
 
     const priceFilter = {
       min_price: priceFrom.value ? Math.max(0, parseFloat(priceFrom.value)) : 0,
-      max_price: priceTo.value ? Math.max(0, parseFloat(priceTo.value)) : Infinity,
+      max_price: priceTo.value
+        ? Math.max(0, parseFloat(priceTo.value))
+        : Infinity,
     };
-
-    dispatch(filterPriceAction(priceFilter));
+    if (favoritesPage) {
+      dispatch(filterFavoritesPriceAction(priceFilter));
+    } else dispatch(filterPriceAction(priceFilter));
     e.target.reset();
   };
 
@@ -35,8 +42,8 @@ function FilterProducts({ schowSaleFilter }) {
     >
       <form className={styles.priceInputs} onSubmit={filter}>
         <label htmlFor="priceFrom">Price</label>
-        <input type="number" name="priceFrom" placeholder="from" min={0}/>
-        <input type="number" name="priceTo" placeholder="to" min={0}/>
+        <input type="number" name="priceFrom" placeholder="from" min={0} />
+        <input type="number" name="priceTo" placeholder="to" min={0} />
         <button className={styles.filterButton} type="submit"></button>
       </form>
 
@@ -60,7 +67,11 @@ function FilterProducts({ schowSaleFilter }) {
           name="sortSelect"
           className={styles.select}
           style={{ backgroundImage: `url(${arrowDown})` }}
-          onChange={(e) => dispatch(sortProductsAction(e.target.value))}
+          onChange={(e) =>
+            favoritesPage
+              ? dispatch(sortFavoritesAction(e.target.value))
+              : dispatch(sortProductsAction(e.target.value))
+          }
         >
           <option value="default">by default</option>
           <option value="newest">Newest</option>
