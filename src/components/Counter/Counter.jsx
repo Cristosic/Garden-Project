6e4cfb00d3585ccb47c3c60eccb8ded17d18fe44
Cristina <acristina4040@gmail.com>
2024-06-03 01:33:37 +0200

    add dark theme for Counter, FilterProducts buttons, SectionSale buttons hover, FavoriteItemPage, SingleProductPage

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { increase, decrease } from "../../store/slices/counterSlice";
 import styles from './Counter.module.css';
+import { Context } from '../../context';
 
 const Counter = () => {
+
+  const { theme } = useContext(Context);
   const counter = useSelector((state) => state.counter.counter);
   const dispatch = useDispatch();
 
   return (
-    <div className={styles.ProductCounterContainer}>
-      <button className={styles.quantityButton} onClick={() => dispatch(decrease())}>-</button>
+    <div
+      className={`${styles.productCounterContainer} ${
+        theme === "light" ? styles.lightTheme : styles.darkTheme
+      }`}
+    >
+      <button
+        className={styles.quantityButton}
+        onClick={() => dispatch(decrease())}
+      >
+        -
+      </button>
       <div className={styles.quantity}>{counter}</div>
-      <button className={styles.quantityButton} onClick={() => dispatch(increase())}>+</button>
+      <button
+        className={styles.quantityButton}
+        onClick={() => dispatch(increase())}
+      >
+        +
+      </button>
     </div>
   );
 };
