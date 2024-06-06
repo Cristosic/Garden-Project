058e3efa74ae4266f7b1308cf638227a6b5f7923
Cristina <acristina4040@gmail.com>
2024-06-07 01:02:27 +0200

    dark theme for skeleton

--- src/components/Skeleton/Skeleton.jsx
+++ src/components/Skeleton/Skeleton.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../Skeleton/Skeleton.module.css";
 import { useSelector } from "react-redux";
 import renderSkeletonSquares from "./../../utils/renderSkeletonSquares";
+import { Context } from "../../context";
 
 const Skeleton = () => {
+
+    const { theme } = useContext(Context);
+
   const loading = useSelector((state) => state.allProducts.status) === "loading";
   console.log('Loading state in Skeleton:',loading)
 
   if (loading) {
     return (
-      <div className={styles["skeleton-container"]}>
+      <div
+        className={`${styles["skeleton-container"]} ${
+          theme === "light" ? styles.lightTheme : styles.darkTheme
+        }`}
+      >
         <div className={styles["skeleton-grid"]}>
           {renderSkeletonSquares(11, styles["skeleton-square"])}
         </div>
