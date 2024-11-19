e443c415c80830ce416d4ca477a014e4c7a5044b
Cristina <acristina4040@gmail.com>
2024-11-19 21:20:19 +0100

    add some changes in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import styles from "./Header.module.scss"
-import banner from "../../media/images/banner.svg"
-import { Link } from 'react-router-dom';
+import React from "react";
+import styles from "./Header.module.scss";
+import banner from "../../media/images/banner.svg";
+import { Link } from "react-router-dom";
 function Header() {
   return (
-    <div className={styles.banner} >
-      <img className={styles.headerImg} src={banner} alt="" />
+    <div className={styles.banner}>
+      <img className={styles.headerImg} src={banner} alt="garden-image" />
 
       <div className={styles.headerContent}>
-        <h1 className={styles.title}>Amazing Discounts <span>on Garden Products!</span> </h1>
+        <h1 className={styles.title}>Amazing Discounts on Garden Products!</h1>
         <div>
-          <Link to="/sales" >
+          <Link to="/sales">
             <button className={styles.button}>Check out</button>
           </Link>
         </div>
-
       </div>
     </div>
-
-
-
-
-  )
+  );
 }
 
-export default Header
+export default Header;
